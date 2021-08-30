5225d4065d3be487716a27e40543bfc8d538c6fe
Ryan Clark <ryanclark2k@gmail.com>
2021-08-29 22:43:55 -0700

    Update about

--- src/js/base/about.tsx
+++ src/js/base/about.tsx
@@ -6,13 +6,19 @@ const About = () => {
   const { t } = useTranslation()
 
   const summary = `This site strives to be on the cutting edge of frontend technology,
-    utilizing the latest and greatest from React, Redux, Babel, Webpack, Material UI, and Bootstrap.`
+    utilizing the latest and greatest from React, Redux, Babel, Webpack, Material UI, and Bootstrap.
+    You can view the source code at `
 
   return (
     <Row>
       <Col xs="6">
         <h1>{t("About this site")}</h1>
-        <p>{t(summary)}</p>
+        <p>
+          {t(summary)}
+          <a target="_blank" rel="noreferrer" href="https://github.com/RyanClark2k/web">
+            https://github.com/RyanClark2k/web
+          </a>
+        </p>
       </Col>
     </Row>
   )
