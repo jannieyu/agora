21d07e9e7608d549b78d4bcce07780092d13c0fb
Ryan Clark <ryanclark2k@gmail.com>
2021-08-29 22:17:21 -0700

    Update navbar

--- src/js/base/index.tsx
+++ src/js/base/index.tsx
@@ -18,15 +18,14 @@ const Base = (props: BaseProps) => {
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Hello World!</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      {children}
+      <Container>{children}</Container>
     </>
   )
 }
