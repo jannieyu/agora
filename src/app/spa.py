4bf22358e7fdef3e1ce4ca83feb3b187935d515a
Ryan Clark <ryanclark2k@gmail.com>
2021-09-05 15:27:13 -0700

    Fix SPA typing

--- src/app/spa.py
+++ src/app/spa.py
@@ -6,10 +6,10 @@ import os
 class SPA(StaticFiles):
     """Routes all unmatched requests to static/index.html"""
 
-    def __init__(self, directory: str, index="index.html") -> None:
+    def __init__(self, directory: str, index: str = "index.html") -> None:
         self.index = index
 
-        super().__init__(directory=directory, packages=None, html=True, check_dir=True)
+        super().__init__(directory=directory, packages=[], html=True, check_dir=True)
 
     async def lookup_path(self, path: str) -> tuple[str, Optional[os.stat_result]]:
         full_path, stat_result = await super().lookup_path(path)
