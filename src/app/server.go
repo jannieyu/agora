361f0dd4712e5e80eef2cd012d9ac67167ffe419
Ryan Clark <ryanclark2k@gmail.com>
2022-03-30 10:32:24 -0700

    Add GORM

--- src/app/server.go
+++ src/app/server.go
@@ -5,9 +5,34 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	ID        uint32
+	FirstName string
+	LastName  string
+	Email     string
+	Pword     string
+}
+
 func main() {
+
+	dsn := "host=localhost user=postgres password=postgres dbname=web port=5432 sslmode=disable TimeZone=US/Pacific"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	var users []User
+	db.Model(&User{}).Select("*").Scan(&users)
+
+	for _, user := range users {
+		fmt.Println("User", user.FirstName, user.LastName, "has registered with email", user.Email)
+	}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
