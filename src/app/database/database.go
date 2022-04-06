c3f9c1f3a030dd5ecd4ccffbbcfe7bfb74f551bd
Jannie Yu <jannie@caltech.edu>
2022-04-05 20:35:58 -0700

    add backend

--- src/app/database/database.go
+++ src/app/database/database.go
@@ -6,15 +6,21 @@ import (
 	"gorm.io/gorm"
 )
 
-func Init() *gorm.DB {
+func Init() (*gorm.DB, error) {
 	// Password should be set as EVN variable rather than hardcoded value
 	dsn := "host=localhost user=postgres password=postgres dbname=web port=5432 sslmode=disable TimeZone=US/Pacific"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.WithError(err).Error("Failed to connect to database.")
+		return nil, err
 	}
-	if err := db.AutoMigrate(&User{}); err != nil {
-		log.WithError(err).Error("Failed to initiate Users table.")
-	}
-	return db
+	//if err := db.AutoMigrate(&User{}); err != nil {
+	//	log.WithError(err).Error("Failed to initiate Users table.")
+	//	return nil, err
+	//}
+	//if err := db.AutoMigrate(&Item{}); err != nil {
+	//	log.WithError(err).Error("Failed to initiate Items table.")
+	//	return nil, err
+	//}
+	return db, nil
 }
