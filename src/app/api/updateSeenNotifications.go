f55601a19eebe921a73a1328741b05076bc3ba72
Jannie Yu <jannie@caltech.edu>
2022-04-26 00:56:37 -0700

    Add seen boolean to notifications

---
+++ src/app/api/updateSeenNotifications.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func (h Handle) UpdateSeenNotifications(w http.ResponseWriter, r *http.Request) {
+	session, err := h.Store.Get(r, "user-auth")
+	if err != nil {
+		log.WithError(err).Error("Failed to get cookie session for login status check.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	receiverId := session.Values["id"].(uint32)
+
+	urlParams := r.URL.Query()["data"][0]
+	var payload map[string][]uint32
+	if err := json.Unmarshal([]byte(urlParams), &payload); err != nil {
+		log.WithError(err).Error("Failed to unmarshal bid information.")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var noteReceiverIds []uint32
+	if err := h.Db.Table("notifications").Select("receiver_id").Where("id IN ?", payload["noteIds"]).Find(&noteReceiverIds).Error; err != nil {
+		log.WithError(err).Error("Failed to query notifications to be marked as seen.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	for _, r := range noteReceiverIds {
+		if r != receiverId {
+			log.Error(fmt.Sprintf("Logged-in user %d does not match notification recipient %d.", receiverId, r))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	if err = h.Db.Table("notifications").Where("id IN ?", payload["noteIds"]).Updates(map[string]interface{}{"seen": true}).Error; err != nil {
+		log.WithError(err).Error("Failed to update notifications as seen.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	SafeEncode(w, "{}")
+}
