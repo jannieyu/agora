5da4659446cbf549a1c04d22e2c3a1e2865bd7cf
Jannie Yu <jannie@caltech.edu>
2022-04-26 16:46:32 -0700

    Update payload for retrieving user bids

--- src/app/api/getBids.go
+++ src/app/api/getBids.go
@@ -2,29 +2,45 @@ package api
 
 import (
 	"agora/src/app/database"
-	"encoding/json"
-	"net/http"
-
+	"agora/src/app/user"
+	"github.com/shopspring/decimal"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
-func (h Handle) GetBids(w http.ResponseWriter, r *http.Request) {
-	urlParams := r.URL.Query()["data"][0]
-	payload := struct {
-		itemId uint32
-	}{}
-	if err := json.Unmarshal([]byte(urlParams), &payload); err != nil {
-		log.WithError(err).Error("Failed to unmarshal item id for getting item bids.")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+type ItemBidsAPI struct {
+	ID             uint32          `json:"itemId,omitempty"`
+	Name           string          `json:"itemName,omitempty"`
+	Image          string          `json:"itemImage,omitempty"`
+	HighestBid     decimal.Decimal `json:"highestItemBid,omitempty"`
+	HighestUserBid decimal.Decimal `json:"highestUserBid,omitempty"`
+}
 
-	var bids []database.Bid
-	result := h.Db
-	if err := result.Select("bid_price", "bidder_id", "created_at").Where(&database.Bid{ItemID: payload.itemId}).Find(&bids).Error; err != nil {
-		log.WithError(err).Error("Failed to make query to get item bids.")
+func (h Handle) GetBids(w http.ResponseWriter, r *http.Request) {
+	bidderId, err := user.GetAuthorizedUserId(h.Store, r)
+	if err != nil {
+		log.WithError(err).Error("Failed to get cookie session when authorized user id.")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	SafeEncode(w, bids)
+	if bidderId == 0 {
+		log.Error("Cannot access user bids without login.")
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	var result []ItemBidsAPI
+	if err := h.Db.Model(&database.Bid{}).Select(
+		"items.id, "+
+			"items.name, "+
+			"items.highest_bid, "+
+			"items.image, "+
+			"max(bids.bid_price) as highest_user_bid").Group(
+		"items.id").Joins(
+		"left join items on items.id = bids.item_id").Where(
+		"bids.bidder_id = ?", bidderId).Find(&result).Error; err != nil {
+		log.WithError(err).Error("Failed to query user bid items.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	SafeEncode(w, result)
 }
