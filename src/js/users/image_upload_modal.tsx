378612f527f901c943529a630d32e85b0158e7da
Ryan Clark <ryanclark2k@gmail.com>
2022-05-01 22:20:26 -0700

    Start work on croppable image uploader

--- src/js/users/image_upload_modal.tsx
+++ src/js/users/image_upload_modal.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
-import { Modal } from "react-bootstrap"
+import { Row, Modal } from "react-bootstrap"
+import Dropzone from "react-dropzone"
+import { useCallback, useState } from "../base/react_base"
 
 interface ImageUploadModalProps {
   show: boolean
@@ -9,10 +11,29 @@ interface ImageUploadModalProps {
 export default function ImageUploadModal(props: ImageUploadModalProps) {
   const { show, onHide } = props
 
+  const [image, setImage] = useState<File | null>(null)
+  const [imageURL, setImageURL] = useState<string>("")
+
+  const hideAndReset = useCallback(() => {
+    setImage(null)
+    setImageURL("")
+    onHide()
+  }, [onHide])
+
+  const handleChangeImage = useCallback((files: File[]) => {
+    const imageFile = files[0]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      setImageURL(e.target.result as string)
+    }
+    reader.readAsDataURL(imageFile)
+    setImage(imageFile)
+  }, [])
+
   return (
     <Modal
       show={show}
-      onHide={onHide}
+      onHide={hideAndReset}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
@@ -20,7 +41,23 @@ export default function ImageUploadModal(props: ImageUploadModalProps) {
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">Upload Image</Modal.Title>
       </Modal.Header>
-      <Modal.Body>IMAGE UPLOAD HERE</Modal.Body>
+      <Modal.Body>
+        <Row>
+          <Dropzone onDrop={handleChangeImage} accept={{ "image/*": [".jpeg", ".png", ".gif"] }}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <div className="droparea">
+                  <input {...getInputProps()} />
+                  <b className="droparea-text">
+                    {image?.name || "Drag and drop an image of the listed item, or click to upload"}
+                  </b>
+                </div>
+              </div>
+            )}
+          </Dropzone>
+        </Row>
+        <Row>{image && <img src={imageURL} alt="User Profile" />}</Row>
+      </Modal.Body>
     </Modal>
   )
 }
