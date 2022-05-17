43ab63ccc70fe76623901c57b65a8fc5281cf0cb
Ryan Clark <ryanclark2k@gmail.com>
2022-05-17 14:48:14 -0700

    Add AuctionHouse branding

--- src/js/base/about.tsx
+++ src/js/base/about.tsx
@@ -5,14 +5,15 @@ function About() {
   return (
     <Row>
       <Col xs="6">
-        <h1>About Agora</h1>
+        <h1>About AuctionHouse</h1>
         <p>
-          Agora is a hyperlocal auction platform. Users can create auctions that run for a preset
-          amount of time and invite members of their community to participate. For example, a
-          university can create an Agora auction in the spring in which graduating students list
-          items to sell to underclassmen. Agora is a trust based platform: we do not collect credit
-          card information and <i>cannot enforce </i> enforce that a transaction is completed after
-          the auction ends. Make sure to only invite trusted community members to Agora auctions!
+          AuctionHouse is a hyperlocal auction platform. Users can create auctions that run for a
+          preset amount of time and invite members of their community to participate. For example, a
+          university can create an AuctionHouse auction in the spring in which graduating students
+          list items to sell to underclassmen. AuctionHouse is a trust based platform: we do not
+          collect credit card information and <i>cannot enforce </i> enforce that a transaction is
+          completed after the auction ends. Make sure to only invite trusted community members to
+          AuctionHouse auctions!
         </p>
       </Col>
     </Row>
